f0fce7a7180ae7ff9d48b8afcc4b9ca82a10eef1
Samuca401 <matheuslealbm@gmail.com>
2024-10-31 00:31:05 -0300

    teste

--- Scripts/home.js
+++ Scripts/home.js
@@ -14,54 +14,60 @@ const elementosDaPaginaHome = {
     JanelaParaCriarTarefa: () => document.querySelector('.janelaParaCriarTarefa'),
     caixaDeTarefas: () => document.querySelector('.caixaDeTarefas'),
     aba: () => document.querySelector('.aba'),
+    inputNomeDaTarefa: () => document.querySelector('#input-nome-tarefa'),
 }
 
+const localStorageKey = "tarefasDoTaskBag";
 
-// MOTOR DO PRODUCTION
-const localStorageKey = "line-tasks";
-
-function newTask() {
-    if (!inputContentTask.value) {
-        alert("digite algo no campo de digitação de task");
+function novaTarefa() {
+    if (!elementosDaPaginaHome.inputNomeDaTarefa()) {
+        alert("Por favor infome o nome da tarefa");
     } else {
-        let arrayTasks = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
-        arrayTasks.push({
-            title: inputContentTask.value
+        let arrayTarefas = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+        arrayTarefas.push({
+            title: elementosDaPaginaHome.inputNomeDaTarefa().value
         });
-        localStorage.setItem(localStorageKey,JSON.stringify(arrayTasks));
-        showTasks();
-        inputContentTask.value = "";
+        localStorage.setItem(localStorageKey,JSON.stringify(arrayTarefas));
+        carrregarTarefas();
+        elementosDaPaginaHome.inputNomeDaTarefa().value = "";
     }
 }
 
-function showTasks() {
-    let arrayTasks = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
-    divListTask.innerHTML = "";
-    for (let iterador = 0;iterador < arrayTasks.length;iterador++) {
-        const div = document.createElement('div');
-        div.setAttribute('class','task');
+function carrregarTarefas() {
+    console.log('funcionando...');
+    let arrayTarefas = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+    elementosDaPaginaHome.caixaDeTarefas().innerHTML = "";
 
-        const titleTask = document.createElement('p');
-        titleTask.innerHTML = `${arrayTasks[iterador]['title']}`;
-
-        const deleteButton = document.createElement('button');
-        deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
-        deleteButton.addEventListener("click",()=>{
-            deleteTask(arrayTasks[iterador]['title']);
-        });
-
-        div.appendChild(titleTask);
-        div.appendChild(deleteButton);
-        divListTask.appendChild(div);
+    if (arrayTarefas.length == 0) {
+        elementosDaPaginaHome.caixaDeTarefas().innerHTML = "Tarefas Aqui";
+    } else {
+        for (let iterador = 0;iterador < arrayTarefas.length;iterador++) {
+            const div = document.createElement('div');
+            div.setAttribute('class','tarefa');
+    
+            const nomeDaTarefa = document.createElement('p');
+            nomeDaTarefa.innerHTML = `${arrayTarefas[iterador]['title']}`;
+    
+            const deletar = document.createElement('button');
+            deletar.innerHTML = '<i class="fa-solid fa-trash"></i>';
+            deletar.addEventListener("click",()=>{
+                deletaTarefa(arrayTarefas[iterador]['title']);
+            });
+    
+            div.appendChild(nomeDaTarefa);
+            div.appendChild(deletar);
+            elementosDaPaginaHome.caixaDeTarefas().appendChild(div);
+        }
     }
+
 }
 
-function deleteTask(data) {
-    let arrayTasks = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
-    let index = arrayTasks.findIndex(x => x.title == data);
-    arrayTasks.splice(index, 1);
-    localStorage.setItem(localStorageKey, JSON.stringify(arrayTasks));
-    showTasks();
+function deletaTarefa(data) {
+    let arrayTarefas = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+    let index = arrayTarefas.findIndex(x => x.title == data);
+    arrayTarefas.splice(index, 1);
+    localStorage.setItem(localStorageKey, JSON.stringify(arrayTarefas));
+    carrregarTarefas();
 }
 
-// window.addEventListener("load",showTasks);
+window.addEventListener("load",carrregarTarefas);
