36ef5c21c5210881f152de128500d12eb0b57f45
Samucaa <160365506+s4muc473@users.noreply.github.com>
2024-11-25 14:34:43 -0300

    Update microbloco.js

--- Scripts/microbloco.js
+++ Scripts/microbloco.js
@@ -4,6 +4,16 @@ const elementosDaPagina = {
     caixaDeSolicitacoes: () => document.querySelector('.solicitacoes'),
 }
 
+function addSubBloco(event) {
+    const tecla = event.key; 
+      
+    if (tecla === 'Enter') {
+        novaSolicitacao();
+    }
+}
+
+window.addEventListener('keydown', addSubBloco);
+
 const localStorageKey = "tarefasDoTaskBag"; // CHAVE DA API
 let arrayTarefas = JSON.parse(localStorage.getItem(localStorageKey) || "[]"); // PEGA OS DADOS DA API
 let id = localStorage.idDaTarefa;
